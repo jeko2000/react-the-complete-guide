aac238b7a601e107376863549526bbc5c46be00a
Johnny Ruiz <jruiz@mini.local>
2019-11-21 08:14:41 -0500

    Complete 32. Creating a functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <hi>Hi, I am a React App</hi>
+        <p>I'm a person</p>
+        <Person />
       </div>
     );
     /* return React.createElement('div', {className: 'App'},
