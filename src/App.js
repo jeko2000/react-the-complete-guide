c244ea11311b9cfdf29dcceb58decee4ac717ce8
Johnny Ruiz <jruiz@mini.local>
2019-11-22 07:57:49 -0500

    Complete 54. Handling dynamic content 'The Javascript Way'

--- src/App.js
+++ src/App.js
@@ -48,27 +48,32 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person name={this.state.persons[0].name}
+                  age={this.state.persons[0].age}/>
+          <Person name={this.state.persons[1].name}
+                  age={this.state.persons[1].age}
+                  click={this.switchNameHandler.bind(this, 'Max!')}
+                  changed={this.nameChangedHandler}>My hobbies: Racing</Person>
+          // bind method is more efficient
+          <Person name={this.state.persons[2].name}
+                  age={this.state.persons[2].age}/>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really working!</p>
         <button
           style={style}
-          onClick={this.togglePersonsHandler}>Toggle names</button>
-        {
-          this.state.showPersons ?
-          <div>
-            <Person name={this.state.persons[0].name}
-                    age={this.state.persons[0].age}/>
-            <Person name={this.state.persons[1].name}
-                    age={this.state.persons[1].age}
-                    click={this.switchNameHandler.bind(this, 'Max!')}
-                    changed={this.nameChangedHandler}>My hobbies: Racing</Person>
-            // bind method is more efficient
-            <Person name={this.state.persons[2].name}
-                    age={this.state.persons[2].age}/>
-          </div> : null
-        }
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
     /* return React.createElement('div', {className: 'App'},
