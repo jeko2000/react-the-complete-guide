e1ff74262c497e833414807065d97c159d70d4d7
Johnny Ruiz <jruiz@mini.local>
2019-11-22 08:16:19 -0500

    Complete 57. Lists & State

--- src/App.js
+++ src/App.js
@@ -15,18 +15,6 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = () => {
-    //console.log('Was clicked');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    })
-  }
-
   nameChangedHandler = (event) => {
     this.setState( {
       persons: [
@@ -40,6 +28,13 @@ class App extends Component {
   togglePersonsHandler = () => {
     this.setState({showPersons: !this.state.showPersons});
   }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -54,8 +49,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person
+                     click={() => this.deletePersonHandler(index)}
                      name={person.name}
                      age={person.age} />
           })}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,8 +6,7 @@ const person = (props) => {
   /* I want the X below to be a random number  */
   return (
     <div className="Person">
-      <p>I'm a {props.name} and I am {props.age} years old!</p>
-      <p onClick={props.click}>{props.children}</p>
+      <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
       <input type="text" onChange={props.changed} value={props.name} />
     </div>
   )
