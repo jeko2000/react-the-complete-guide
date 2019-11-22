cbf75000c31dc6f80a7771b0b2ac7b82a6b1d671
Johnny Ruiz <jruiz@mini.local>
2019-11-21 21:32:21 -0500

    Complete 48. Adding styles with stylesheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './Person.css';
+
 const person = (props) => {
   /* I want the X below to be a random number  */
   return (
-    <div>
+    <div className="Person">
       <p>I'm a {props.name} and I am {props.age} years old!</p>
       <p onClick={props.click}>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
