ee602956b2d434227f49fede901a882cc1dc4479
Johnny Ruiz <jruiz@mini.local>
2019-11-24 21:11:28 -0500

    Complete 69. Using Radium for media queries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -89,14 +89,20 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I am a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+      /*
+         We wrap the entire node in StyleRoot unless we only
+         need sudo-selectors from Radium
+       */
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I am a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
     /* return React.createElement('div', {className: 'App'},
      *                            React.createElement('h1', null, 'Hello from createElement')); */
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,9 +4,14 @@ import Radium from 'radium';
 import './Person.css';
 
 const person = (props) => {
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  }
   /* I want the X below to be a random number  */
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
       <input type="text" onChange={props.changed} value={props.name} />
     </div>
